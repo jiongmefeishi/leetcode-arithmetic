3f862b9db755df017f8cf7865e5db60ee0177cfc
zqtao <zqtao23@163.com>
2021-06-08 15:55:17 +0800

    Create FindMaxConsecutiveOnes.java

---
+++ src/main/java/me/arithmetic/array/FindMaxConsecutiveOnes.java
@@ -0,0 +1,15 @@
+package me.arithmetic.array;
+
+/**
+ * @author: 囧么肥事
+ * @date: 2021/6/8 15:40
+ * @description: 485. 最大连续 1 的个数
+ */
+public class FindMaxConsecutiveOnes {
+
+    public int findMaxConsecutiveOnes(int[] nums) {
+
+        int maxCount = 0;
+        int count = 0;
+    }
+}
