99da1dfc85750c6901d1583657413a9458488149
zqtao <zqtao23@163.com>
2021-06-08 16:50:27 +0800

    485. 最大连续 1 的个数

--- src/main/java/me/arithmetic/array/FindMaxConsecutiveOnes.java
+++ src/main/java/me/arithmetic/array/FindMaxConsecutiveOnes.java
@@ -7,9 +7,30 @@ package me.arithmetic.array;
  */
 public class FindMaxConsecutiveOnes {
 
-    public int findMaxConsecutiveOnes(int[] nums) {
+    public static int findMaxConsecutiveOnes(int[] nums) {
 
+        // 记录最大个数
         int maxCount = 0;
+        // 记录连续1个数
         int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                // 取最大连续数
+                maxCount = Math.max(maxCount, count);
+                // 重置连续个数
+                count = 0;
+            } else {
+                count++;
+            }
+        }
+        maxCount = Math.max(maxCount, count);
+        return maxCount;
     }
+
+    public static void main(String[] args) {
+        int[] array = {1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0};
+        System.out.println(findMaxConsecutiveOnes(array));
+    }
+
 }
