a6333beadab26aa1bb226ed8a8f000d3ef666110
zqtao <zqtao23@163.com>
2021-06-24 18:03:29 +0800

    Update FirstMissingPositive.java

--- src/main/java/me/arithmetic/array/FirstMissingPositive.java
+++ src/main/java/me/arithmetic/array/FirstMissingPositive.java
@@ -9,6 +9,21 @@ public class FirstMissingPositive {
 
     public int firstMissingPositive(int[] nums) {
 
+        // 缺失数一定出现在 [1, N+1] 范围内
+        // 取得数组长度为 N
+        // 第一次遍历，将所有非正数修改为 N+1
+        int N = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                nums[i] = N + 1;
+            }
+        }
+
+        // 第二次遍历，打标记，将元素属于 1 ~ N 范围内的数反转
+        for (int i = 0; i < nums.length; i++) {
+            
+        }
+
         return 0;
     }
 }
