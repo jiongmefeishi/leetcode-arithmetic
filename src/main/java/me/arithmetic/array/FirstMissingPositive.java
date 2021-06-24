b3f1a45662d4f2b22b1ddb2e67d32221e3ad14c1
zqtao <zqtao23@163.com>
2021-06-24 15:14:52 +0800

    41. 缺失的第一个正数

---
+++ src/main/java/me/arithmetic/array/FirstMissingPositive.java
@@ -0,0 +1,43 @@
+package me.arithmetic.array;
+
+/**
+ * @author: zqtao
+ * @date: 2021/6/24 14:45
+ * @description: 41. 缺失的第一个正数
+ */
+public class FirstMissingPositive {
+
+    public int firstMissingPositive(int[] nums) {
+
+        return 0;
+    }
+}
+
+/*
+给你一个未排序的整数数组 nums ，请你找出其中没有出现的最小的正整数。
+
+请你实现时间复杂度为 O(n) 并且只使用常数级别额外空间的解决方案。
+ 
+
+示例 1：
+
+输入：nums = [1,2,0]
+输出：3
+示例 2：
+
+输入：nums = [3,4,-1,1]
+输出：2
+示例 3：
+
+输入：nums = [7,8,9,11,12]
+输出：1
+
+提示：
+
+1 <= nums.length <= 5 * 105
+-231 <= nums[i] <= 231 - 1
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/first-missing-positive
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
