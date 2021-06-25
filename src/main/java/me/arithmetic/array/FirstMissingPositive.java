a9b4fd58cdb8cb647da492243b5dfa4529c30b9b
zqtao <zqtao23@163.com>
2021-06-25 10:18:37 +0800

    41. 缺失的第一个正数

--- src/main/java/me/arithmetic/array/FirstMissingPositive.java
+++ src/main/java/me/arithmetic/array/FirstMissingPositive.java
@@ -7,24 +7,41 @@ package me.arithmetic.array;
  */
 public class FirstMissingPositive {
 
-    public int firstMissingPositive(int[] nums) {
+    public static int firstMissingPositive(int[] nums) {
 
         // 缺失数一定出现在 [1, N+1] 范围内
         // 取得数组长度为 N
         // 第一次遍历，将所有非正数修改为 N+1
         int N = nums.length;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < 0) {
+            if (nums[i] <= 0) {
                 nums[i] = N + 1;
             }
         }
 
-        // 第二次遍历，打标记，将元素属于 1 ~ N 范围内的数反转
-        for (int i = 0; i < nums.length; i++) {
-            
+        // 第二次遍历，打标记，将元素属于 1 ~ N 范围内的数反转为负数
+        for (int n : nums) {
+            if (Math.abs(n) <= N) {
+                nums[Math.abs(n) - 1] = - Math.abs(nums[Math.abs(n) - 1]) ;
+            }
         }
 
-        return 0;
+        // 第三次遍历，元素大于0，则缺失数为 下标 + 1
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                return i  + 1;
+            }
+        }
+
+        return N + 1;
+    }
+
+    public static void main(String[] args) {
+
+//        int[] nums = {3, 4, -1, 1};
+        int[] nums = {1,1};
+        System.out.println(firstMissingPositive(nums));
+
     }
 }
 
