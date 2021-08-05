cd6333d3f95123bcf81a3672d3165944dcf166f9
zqtao <zqtao23@163.com>
2021-08-05 17:59:24 +0800

    暴力递归

---
+++ src/main/java/me/arithmetic/dp/MinDistance3.java
@@ -0,0 +1,25 @@
+package me.arithmetic.dp;
+
+
+/**
+ * @author: zqtao
+ * @date: 2021/8/4 15:25
+ * @description: 72. 编辑距离
+ */
+public class MinDistance3 {
+
+    // 动态规划
+    public static int minDistance(String word1, String word2) {
+
+    }
+
+    public static void main(String[] args) {
+
+//        String str1 = "zh1an2g";
+//        String str2 = "zhang";
+        String str1 = "intention";
+        String str2 = "execution";
+        System.out.println(minDistance(str1, str2));
+    }
+}
+
